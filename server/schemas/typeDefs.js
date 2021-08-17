a9660397b41476ba93bb9cf8aeeec59dfff2b40f
Pinell <rpinell@gwelec.com>
2021-08-17 11:09:03 -0500

    Added operation model and schema

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -68,6 +68,13 @@ const typeDefs = gql `
     datetime: Date!
   }
 
+  type Operation {
+    operationId: ID!
+    description: String!
+    user: String!
+    datetime: Date!
+  }
+
   type User {
     _id: ID!
     username: String!
