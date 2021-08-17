3489eabf9685729c5af6339dedf96923e18d8b9e
Pinell <rpinell@gwelec.com>
2021-08-17 12:59:52 -0500

    Added model exports in models/index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,17 @@
+//
+//  Export models required throughout the application
+//
+const Category = require('./Category');
+const Conversion = require('./Conversion');
+const Item = require('./Item');
+const Kardex = require('./Kardex');
+const Location = require('./Location');
+const Operation = require('./Operation');
+const OrderDetail = require('./OrderDetail');
+const OrderHeader = require('./OrderHeader');
+const Role = require('./Role');
+const Task = require('./Task');
+const UoM = require('./UoM');
 const User = require('./User');
 
-module.exports = { User };
+module.exports = { Category, Conversion, Item, Kardex, Location, Operation, OrderDetail, OrderHeader, Role, Task, UoM, User};
