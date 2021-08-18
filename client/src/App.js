3eb2e1fe80fd36558e4aaefd857742c3d05b3bac
gannacon <cwgannaway@gmail.com>
2021-08-17 21:40:43 -0500

    starting theme colors inside of appjs

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,25 @@ import Dispatch from "./pages/Dispatch";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
+import { createTheme, ThemeProvider, Button } from "@material-ui/core";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#ed4b82",
+      main: "#e91e63",
+      dark: "#a31545",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#33eaff",
+      main: "#00e5ff",
+      dark: "#00a0b2",
+      contrastText: "#000",
+    },
+  },
+});
+
 // Construct the main GraphQL API endpoint
 const httpLink = createHttpLink({
   uri: "/graphql",
