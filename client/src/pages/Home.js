edefa65fd55b9fe064a542565af85ff590f4e50b
Travis Brown <bergannation@gmail.com>
2021-08-18 19:00:57 -0500

    fixed home/profile

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,23 +1,23 @@
-//
-//  Functions to call the search API and execute the mutations to save data to the MongoDb database
-//
-import React, { useState, useEffect } from "react";
-import {
-  Jumbotron,
-  Container,
-  Col,
-  Form,
-  Button,
-  Card,
-  CardColumns,
-} from "react-bootstrap";
+import React, { useState } from "react";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import Radio from "@material-ui/core/Radio";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import Slider from "@material-ui/core/Slider";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import SignUpForm from "../components/SignUpForm";
+import { LOGIN_USER } from "../utils/mutations";
 
 import Auth from "../utils/auth";
-import { searchGoogleBooks } from "../utils/API";
-import { saveBookIds, getSavedBookIds } from "../utils/localStorage";
 
-import { useMutation } from "@apollo/client";
-import { SAVE_BOOK } from "../utils/mutations";
+import { useMutation, useQuery } from "@apollo/client";
 
 const Home = () => {
   return (
--- client/src/pages/PutAway.js
+++ client/src/pages/PutAway.js
@@ -1,21 +1,23 @@
-//
-//  Function to retrieve to remove previously saved books from the user account
-//
-import React from "react";
-import {
-  Jumbotron,
-  Container,
-  CardColumns,
-  Card,
-  Button,
-} from "react-bootstrap";
+import React, { useState } from "react";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import Radio from "@material-ui/core/Radio";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import Slider from "@material-ui/core/Slider";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import SignUpForm from "../components/SignUpForm";
+import { LOGIN_USER } from "../utils/mutations";
 
 import Auth from "../utils/auth";
-import { removeBookId } from "../utils/localStorage";
 
 import { useMutation, useQuery } from "@apollo/client";
-import { GET_ME } from "../utils/queries";
-import { REMOVE_BOOK } from "../utils/mutations";
 
 const PutAway = () => {
   return (
