ce4083bdf93869bfe36f2de696512176047622c6
jtstockard <jtstockard92@gmail.com>
2021-08-20 10:48:24 -0500

    Fix user seed data

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -15,7 +15,6 @@ import Link from "@material-ui/core/Link";
 import { useMutation, useQuery } from "@apollo/client";
 import { LOGIN_USER } from "../utils/mutations";
 
-
 const useStyles = makeStyles((theme) => ({
   container: {
     padding: theme.spacing(6),
@@ -51,33 +50,32 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const { userData } = require("../models/User.js");
-
+// const { userData } = require("../models/User.js");
 
 const [formValues, setFormValues] = useState(defaultValues);
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormValues({
-      ...formValues,
-      [name]: value,
-    });
-  };
-  // const handleSliderChange = (name) => (e, value) => {
-  //   setFormValues({
-  //     ...formValues,
-  //     [name]: value,
-  //   });
-  // };
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(formValues);
-  };
+const handleInputChange = (e) => {
+  const { name, value } = e.target;
+  setFormValues({
+    ...formValues,
+    [name]: value,
+  });
+};
+// const handleSliderChange = (name) => (e, value) => {
+//   setFormValues({
+//     ...formValues,
+//     [name]: value,
+//   });
+// };
+const handleSubmit = (event) => {
+  event.preventDefault();
+  console.log(formValues);
+};
 
 const cards = [];
 
 const renderCards = () => {
-  for (const [cards, userData] of userData.entries()) {
+  for (const [i, userData] of userData.entries()) {
     cards.push(<li>{userData}</li>);
 
     return <div>{cards}</div>;
@@ -101,33 +99,45 @@ const Admin = () => {
                     Admin Portal
                   </Typography>
                   <FormControl>
-                    <form className={classes.root} noValidate autoComplete="off"
-                    onSubmit={handleSubmit}>
+                    <form
+                      className={classes.root}
+                      noValidate
+                      autoComplete="off"
+                      onSubmit={handleSubmit}
+                    >
                       <Typography
                         className={classes.pos}
                         color="textSecondary"
                         variant="body2"
                         component="p"
-                        >
+                      >
                         Assign New Task
                       </Typography>
                       <Input
-                        
                         className={classes.CardContent}
                         placeholder="Apply A User"
                         inputProps={{ "aria-label": "description" }}
+                        value={formValues.username}
                         onChange={handleInputChange}
-                        />
+                      />
                       <Input
                         className={classes.CardContent}
                         defaultValue="Select Action"
                         inputProps={{ "aria-label": "description" }}
+                        value={formValues.department}
                         onChange={handleInputChange}
-                        />
+                      />
                     </form>
-                    <Button className={classes.heroButtons} variant="contained" color="primary" type="submit">
-                      Submit
-                    </Button>
+                    <CardActions>
+                      <Button
+                        className={classes.heroButtons}
+                        variant="contained"
+                        color="primary"
+                        type="submit"
+                      >
+                        Submit
+                      </Button>
+                    </CardActions>
                   </FormControl>
                 </Grid>
               </CardContent>
