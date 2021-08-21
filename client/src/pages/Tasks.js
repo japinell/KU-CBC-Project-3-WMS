bd8190d5ab104fca78fdf831f2cb4c2f38c6f416
Travis Brown <bergannation@gmail.com>
2021-08-21 10:15:11 -0500

    updating

--- client/src/pages/Tasks.js
+++ client/src/pages/Tasks.js
@@ -17,16 +17,16 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function createData(name, calories, fat, carbs, protein) {
+function createTaskData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
 }
 
 const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createTaskData("Picking", 159, 6.0, 24, 4.0),
+  createTaskData("Picking 2", 237, 9.0, 37, 4.3),
+  createTaskData("Receiving", 262, 16.0, 24, 6.0),
+  createTaskData("Receiving 2", 305, 3.7, 67, 4.3),
+  createTaskData("Dispatch", 356, 16.0, 49, 3.9),
 ];
 
 export default function Tasks() {
@@ -37,7 +37,7 @@ export default function Tasks() {
       <Table className={classes.table} size="small" aria-label="a dense table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell>Tasks</TableCell>
             <TableCell align="right">Calories</TableCell>
             <TableCell align="right">Fat&nbsp;(g)</TableCell>
             <TableCell align="right">Carbs&nbsp;(g)</TableCell>
